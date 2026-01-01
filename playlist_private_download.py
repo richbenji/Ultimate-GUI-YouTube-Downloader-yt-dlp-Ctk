31d854eff006577efc496c302ec61d09b403f7f3
Richard <rgouy87@gmail.com>
2026-01-01 04:45:27 +0100

    clean origin

--- playlist_private_download.py
+++
@@ -1,6 +0,0 @@
-# Télécharge automatiquement une playlist privée
-yt-dlp --cookies-from-browser firefox https://www.youtube.com/playlist?list=PL599LnXP4B2k6ia66oXHF6Mo8KmjnI3wM
-
-# Télécharge automatiquement une playlist privée avec le fichier cookies.txt
-yt-dlp --cookies ~/Téléchargements/cookies.txt "https://www.youtube.com/playlist?list=PL599LnXP4B2k6mprpX0zodb6PTKpwba1v"
-
